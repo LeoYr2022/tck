2687fabf40d881a117f7446827ad77f7fa560aba
LeoYr2022 <feyntse@outlook.com>
2024-05-29 13:51:24 +0800

    update operate files

--- week5.tcl
+++ week5.tcl
@@ -9,3 +9,21 @@ if {[catch {set fileId [open $filePath r]} errMsg]} {  ;#catch gets the return o
 }
 
 close $fileId
+
+puts "***************************write & read a file************************************************"
+## "gets" reads a line
+## "read" reads the whole file
+set fileId [open "week4.tcl" "r"] ;# r/w/a(append)
+while {[gets $fileId line] >= 0} { 
+   puts "new line: $line" 
+}
+close $fileId
+
+set fileId [open "week4.tcl" "r"] ;# r/w/a(append)
+set content [read $fileId]
+puts "The new file's content are: \n$content"
+close $fileId
+
+set fileId [open "./tmp/output.tcl" "w"]
+puts $fileId "Hello World!"
+close $fileId
