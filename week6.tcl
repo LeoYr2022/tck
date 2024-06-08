e0d41c2f5fdc38bf7591ef60675e686f56a303a6
Leo <116138328+LeoYr2022@users.noreply.github.com>
2024-06-08 07:07:22 +0000

    update week6

--- week6.tcl
+++ week6.tcl
@@ -5,7 +5,7 @@ proc divide {a b} {
     return [expr {$a / $b}]
 }
 
-if {[catch {divide 3 0} result option]} {
+if {[catch {divide 3 1} result option]} {
     puts "Error occured: $result"
     puts "Options: $option"
 } else {
@@ -29,3 +29,35 @@ puts "********************************debug hint********************************
 # puts
 # info
 # trace
+#    trace {add|remove} {variable|command} name {read|write|unset} command
+
+proc debugExample {a b} {
+    puts "a = $a, b = $b"
+    set result [expr {$a + $b}]
+    puts "result = $result"
+    return $result
+}
+debugExample 4 5
+
+puts "********************************debug info**************************************"
+puts "Vars  are: \t \n [info vars]"
+puts "Procs are: \t \n [info procs]"
+
+puts "********************************debug trace**************************************"
+proc trace_callback {name element op} {
+    puts "Variable $name was accedssed for $op"
+}
+trace add variable myVar write trace_callback
+trace add variable myVar read  trace_callback
+trace add variable myVar unset trace_callback
+
+set myVar 0
+set myVar 10
+puts $myVar
+unset myVar
+
+trace remove variable myVar write trace_callback
+trace remove variable myVar read  trace_callback
+trace remove variable myVar unset trace_callback
+set myVar 1
+puts $myVar
