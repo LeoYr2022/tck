5ad8310961b428ce2784c0429ba36f8f5856a2fd
Leo <116138328+LeoYr2022@users.noreply.github.com>
2024-11-07 12:55:21 +0000

    update

---
+++ EEknows/for.tcl
@@ -0,0 +1,9 @@
+#!usr/bin/tclsh
+
+set a "hello Tcl world !"
+set b ""
+for {set i 0} {$i < [llength $a]} {incr i 1} {
+    lappend b [lindex $a $i]
+}
+
+puts $b
--- Tcl_and_the_Tk_Toolkit/c2/1.tcl
+++ Tcl_and_the_Tk_Toolkit/c2/1.tcl
@@ -8,4 +8,8 @@ set lastName "Doe"
 set fullName "$firstName $lastName"
 puts $fullName  ;# 输出 "John Doe"
 puts [string length $fullName]
-puts [string index $fullName 0] ;$index从0开始
+puts [string index $fullName 0] ;#index从0开始
+
+puts "请输入你的名字："
+gets stdin name       ;#get stdin是一个命令，类似 set name <用户输入>
+puts "你好, $name!"
